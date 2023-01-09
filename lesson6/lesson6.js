3fd2fdd4d74047247eea8fdebfef8820db25b8da
tanhun <tanhoang0710@gmail.com>
2023-01-09 11:01:52 +0700

    lesson6

---
+++ lesson6/lesson6.js
@@ -0,0 +1,9 @@
+let count = 1 // ok
+
+count == 'name' // ko error
+
+console.log('>>> check count', count)
+
+const names = ['tanhun', 'ts', 'aaaa'] // array chi bao gom string
+
+names.push(25) // error?
---
+++ lesson6/lesson6.ts
@@ -0,0 +1,10 @@
+let count1: number = 1 // ok
+
+// count1 = 'name' // error vi count1 dang la kieu number
+
+console.log('>>> check name', count1)
+
+const names1 = ['tanhun', 'ts', 'aaaa'] 
+// array chi bao gom string
+
+// names1.push(25) // error?
