b62bd476089385c4a51cc66d5857b51dc7edc89f
tanhun <tanhoang0710@gmail.com>
2023-02-17 23:12:36 +0700

    Coding Challenge #1

--- async-jonas/script.js
+++ async-jonas/script.js
@@ -169,3 +169,27 @@ const getCountryData = country => {
 btn.addEventListener('click', () => {
   getCountryData('portugal');
 });
+
+const whereAmI = (lat, lng) => {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You're in ${data.city}, ${data.country}`);
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found ${res.status}`);
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.log(err.message))
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+whereAmI(52.508, 13.381);
