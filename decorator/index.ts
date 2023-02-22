7e3ddc8c3367eb513b112c9f12087e0692e60a67
tanhun <tanhoang0710@gmail.com>
2023-02-22 12:03:32 +0700

    Decorators

--- decorator/index.ts
+++ decorator/index.ts
@@ -8,29 +8,53 @@
 
 // console.log(test()(1, 2));
 
+// function mul(x: number) {
+//   return function (target: any, propertyName: string, descriptor: PropertyDescriptor) {
+//     // gán ra ngoài
+//     const method = descriptor.value;
+
+//     descriptor.value = (...args: any[]) => {
+//       return x * method(...args);
+//     };
+//   };
+// }
+
 //descriptor => thong tin cua method, property ... noi minh dat vao
-function logger(target: any, propertyName: string, descriptor: PropertyDescriptor) {
-  console.log("🚀 ~ file: index.ts:13 ~ logger ~ descriptor:", descriptor.value);
-  descriptor.value = () => {
-    console.log("test");
-  };
-}
 
 // @Controller('test') Class Decorator
-class TestController {
-  // @Post('') Method Decorator
-  // getUser(@Body() x) { Param Decorator
-  // }
-  // @configurable(false)  Accessor Decorators
-  // get x() {
-  // }
+// class TestController {
+// @Post('') Method Decorator
+// getUser(@Body() x) { Param Decorator
+// }
+// @configurable(false)  Accessor Decorators
+// get x() {
+// }
 
-  @logger
-  hello() {
-    console.log("first");
-  }
-}
+// @logger
+// hello() {
+//   console.log("first");
+// }
 
-const test = new TestController();
-// console.log("🚀 ~ file: index.ts:29 ~ test:", test);
-test.hello();
+// @mul(25)
+// add(a: number, b: number) {
+//   return a + b;
+// }
+// }
+
+// const test = new TestController();
+// // console.log("🚀 ~ file: index.ts:29 ~ test:", test);
+// console.log(test.add(1, 2));
+
+const Controller = (prefix: string) => {
+  return (constructor: any) => {
+    return class Test extends constructor {
+      prefix: string = prefix;
+    };
+  };
+};
+
+@Controller("users")
+class TestController {}
+
+const x = new TestController();
+console.log(x);
