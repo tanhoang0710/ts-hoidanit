12df4b6066471bf0fb8e77ac413378c17793f325
tanhun <tanhoang0710@gmail.com>
2023-02-27 17:46:49 +0700

    Functions Accepting Callback Functions

--- function-jonas/script.js
+++ function-jonas/script.js
@@ -24,5 +24,24 @@ const newPassport = (person) => {
   person.passport = Math.trunc(Math.random() * 1000000000000);
 };
 
-newPassport(tanhun);
-checkIn(flight, tanhun);
+// newPassport(tanhun);
+// checkIn(flight, tanhun);
+
+//Higher-Order-Function: argument is a function or return a new function
+
+const oneWord = (str) => str.replace(/ /g, "").toLowerCase();
+
+const upperFirstWord = (str) => {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+const transformer = (str, fn) => {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("javascript is the best!", upperFirstWord);
+transformer("javascript is the best!", oneWord);
