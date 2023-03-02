39ff7a1b222a75f0d103d14277bedd0032ed3d3a
tanhun <tanhoang0710@gmail.com>
2023-03-02 12:25:25 +0700

    Coding challenge #1

--- function-jonas/script.js
+++ function-jonas/script.js
@@ -150,3 +150,49 @@ const addTaxRate = function (rate) {
 
 const addVAT2 = addTaxRate(0, 23);
 console.log(addVAT(300));
+
+// # Coding challenge 1
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JS", "1: Python", "2: Rust", "3: C++"],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+      )
+    );
+    console.log(
+      "🚀 ~ file: script.js:166 ~ registerNewAnswer ~ answer:",
+      answer
+    );
+
+    // Register
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults("string");
+    this.displayResults();
+  },
+  displayResults(type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      // Poll results are 13, 2, 3, 1
+      console.log(`Poll results are ${this.answers.join(", ")}`);
+    }
+  },
+};
+
+// poll.registerNewAnswer();
+
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] });
+
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
